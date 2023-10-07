75cc950b2627b8d145838786d659077f71fe321e
Aman Khan <amanayankhan78@gmail.com>
2023-10-07 18:36:17 +0530

    Dark Mode Admin Panel UI Setup

---
+++ src/components/core/ThemeSwitcher.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useState } from "react";
+import React from "react";
+import { MoonIcon } from "@/icons/MoonIcon";
+import { SunIcon } from "@/icons/SunIcon";
+
+export const ThemeSwitcher = () => {
+  const { theme, setTheme } = useTheme();
+  const [icon, setIcon] = useState(true);
+
+  const currentTheme = theme == "dark" || theme == undefined ? "light" : "dark";
+  const themeChangeHandler = () => {
+    setTheme(currentTheme);
+    setIcon((prevState) => !prevState);
+  };
+
+  return (
+    <>
+      <div
+        className="bg-gray-100 hover:bg-gray-200 dark:bg-transparent h-8 w-8 flex items-center justify-center rounded-xl"
+        onClick={themeChangeHandler}
+      >
+        {icon ? <MoonIcon /> : <SunIcon />}
+      </div>
+    </>
+  );
+};
