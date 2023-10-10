7784e8bb82f17aec1b23329a3d61fa76b1785e8e
Aman Khan <amanayankhan78@gmail.com>
2023-10-10 18:33:26 +0530

    Update GraphCards.jsx

--- src/components/Graphs/GraphCards.jsx
+++ src/components/Graphs/GraphCards.jsx
@@ -4,7 +4,7 @@ import SalesGraph from "./SalesGraph";
 const GraphCards = () => {
   return (
     <>
-      <div className="flex-1 h-[150px] bg-gradient-to-r from-[#664dc9] to-[#664dcb99] m-2 p-[1.5rem]">
+      <div className="flex-1 h-[150px] bg-gradient-to-r from-[#664dc9] to-[#664dcb99] m-2 p-[1.5rem] rounded-md">
         <div className=" h-full flex">
           <div className=" h-full w-[50%]">
             <h6 className=" text-white">Total Sales</h6>
@@ -15,7 +15,7 @@ const GraphCards = () => {
           </div>
         </div>
       </div>
-      <div className="flex-1 h-[150px] bg-gradient-to-r from-[#1d976c] to-[#2fd38a] m-2 p-[1.5rem]">
+      <div className="flex-1 h-[150px] bg-gradient-to-r from-[#1d976c] to-[#2fd38a] m-2 p-[1.5rem] rounded-md">
         <div className=" h-full flex">
           <div className=" h-full w-[50%]">
             <h6 className=" text-white">Total Sales</h6>
@@ -24,7 +24,7 @@ const GraphCards = () => {
           <div className=" h-full w-[50%]"></div>
         </div>
       </div>
-      <div className="flex-1 h-[150px] bg-gradient-to-r from-[#fa5420] to-[#f6a800] m-2 p-[1.5rem]">
+      <div className="flex-1 h-[150px] bg-gradient-to-r from-[#fa5420] to-[#f6a800] m-2 p-[1.5rem] rounded-md">
         <div className=" h-full flex">
           <div className=" h-full w-[50%]">
             <h6 className=" text-white">Total Sales</h6>
@@ -33,7 +33,7 @@ const GraphCards = () => {
           <div className=" h-full w-[50%]"></div>
         </div>
       </div>
-      <div className="flex-1 h-[150px] bg-gradient-to-r from-[#5b73e8] to-[#44c4fa] m-2 p-[1.5rem]">
+      <div className="flex-1 h-[150px] bg-gradient-to-r from-[#5b73e8] to-[#44c4fa] m-2 p-[1.5rem] rounded-md">
         <div className=" h-full flex">
           <div className=" h-full w-[50%]">
             <h6 className=" text-white">Total Tax</h6>
