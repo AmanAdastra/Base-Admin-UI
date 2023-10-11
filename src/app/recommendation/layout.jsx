99373e6960ef946a6acc75437d6a51283416c356
Aman Khan <amanayankhan78@gmail.com>
2023-10-11 11:53:21 +0530

    pages and links for navigation

---
+++ src/app/recommendation/layout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const layout = ({children}) => {
+  return (
+    <div className='h-[400px] '>{children}</div>
+  )
+}
+
+export default layout
---
+++ src/app/settings/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import UsersGraph from '@/components/Graphs/UsersGraph'
+import PageHeader from '@/components/core/PageHeader'
+import GraphCards from '@/components/Graphs/GraphCards'
+const page = () => {
+  return (
+    <div>
+        <PageHeader title={"Settings"}/>
+        <div className='flex flex-wrap'>
+            <GraphCards/>
+        </div>
+        <UsersGraph/>
+    </div>
+  )
+}
+
+export default page
