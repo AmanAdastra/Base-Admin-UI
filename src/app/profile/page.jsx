ed677164f606c2455722a4062aff630b59890c18
Aman Khan <amanayankhan78@gmail.com>
2023-10-13 13:58:01 +0530

    Protected all pages

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,8 +1,24 @@
+"use client"
 import React from 'react'
 import UsersGraph from '@/components/Graphs/UsersGraph'
 import PageHeader from '@/components/core/PageHeader'
 import GraphCards from '@/components/Graphs/GraphCards'
-const page = () => {
+import { useRouter } from 'next/navigation'
+import { useSession } from 'next-auth/react'
+import { useEffect } from 'react'
+import LoadingComponent from '@/components/core/LoadingComponent'
+const Page = () => {
+  const router = useRouter();
+  const { status } = useSession();
+
+  useEffect(() => {
+    if (status != "authenticated") {
+      router.push("/login");
+      return () => {};
+    }
+  }, [status]);
+  if (status == "loading") return <LoadingComponent />;
+
   return (
     <div>
         <PageHeader title={"Profile"}/>
@@ -14,4 +30,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page
